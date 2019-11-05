ce0e4fc0bd0de2c638dc85fe3bb46ce8a0f14c91
Olli Saarinen <>
2019-11-05 18:08:35 +0200

    Fixed build

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/joho/godotenv"
 
-	"masterchef_bot/bocmd/configuration"
+	"masterchef_bot/cmd/configuration"
 )
 
 // init is invoked before main()
@@ -15,7 +15,6 @@ func init() {
 	godotenv.Load()
 }
 
-// Main
 func main() {
 
 	configuration := configuration.Get()
