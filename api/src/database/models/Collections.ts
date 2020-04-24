9c004eb396d8baf33b664394cfb838b67b52d5d1
Olli Saarinen <>
2020-04-24 13:23:00 +0300

    delete

--- api/src/database/models/Collections.ts
+++
@@ -1,9 +0,0 @@
-/**
- *   Constants for mongo collections
- */
-
-export const RECIPE_COLLECTION = 'recipes';
-export const USER_COLLECTION = 'users';
-
-export const RECIPE = 'Recipe';
-export const USER = 'User';
